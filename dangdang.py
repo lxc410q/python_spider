7485fa15665a68c8efe6d1477dc0af8834428ac2
lxc410q <1161789697@qq.com>
2018-10-21 14:23:45 +0800

    '修改的爬取当当网代码'

--- dangdang.py
+++ dangdang.py
@@ -2,13 +2,15 @@
 #encoding:utf-8
 import re
 import  requests
-import  time
+#import  time
+#headers={'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
+#def res_link(url):
 res=requests.get('http://search.dangdang.com/?key=python&act=input')
 prices=re.findall('<span class="search_now_price">&yen;(.*?)</span>',res.text)
 for price in prices:
-    print(price)
+        print(price)
 #if __name__=='__main__':
- #   urls=['http://search.dangdang.com/?key=python&act=input&page_index={}'.format(number) for number in range(1,52)]
-#for single_url in urls:
-   # res(single_url)
-   # time.sleep(2)
+#    urls=['http://search.dangdang.com/?key=python&act=input&page_index={}'.format(number) for number in range(1,52)]
+#for url in urls:
+#    res_link(urls)
+#    time.sleep(2)
--- dd.py
+++ dd.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 #encoding:utf-8
+import re
+import  requests
+import  time
+res=requests.get('http://search.dangdang.com/?key=python&act=input')
+prices=re.findall('<span class="search_now_price">&yen;(.*?)</span>',res.text)
+for price in prices:
+    print(price)
+#if __name__=='__main__':
+ #   urls=['http://search.dangdang.com/?key=python&act=input&page_index={}'.format(number) for number in range(1,52)]
+#for single_url in urls:
+   # res(single_url)
+   # time.sleep(2)
